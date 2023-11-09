a9bdd014e917c691ed6d25b4934836c6f39c8777
Matt <gr8bitmail@gmail.com>
2023-11-09 12:04:50 -0700

    fix sql insert format

--- CodeComponents/src/init_data/insert.sql
+++ CodeComponents/src/init_data/insert.sql
@@ -1,2 +1,1 @@
-insert into users(username, password) values ('validUsername', '$2a$10$LmCHFRC6LZpuPNj9D9leXueyJicULBRcfVtADUGjOv/XTavy7av22
-');
+insert into users(username, password) values ('validUsername', '$2a$10$LmCHFRC6LZpuPNj9D9leXueyJicULBRcfVtADUGjOv/XTavy7av22');
