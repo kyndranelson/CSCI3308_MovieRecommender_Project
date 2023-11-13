c191697944c7d97266e2b6dab3f06f67b42cf7c7
kyndranelson <143553148+kyndranelson@users.noreply.github.com>
2023-11-13 16:15:38 -0700

    removing duplicate files

--- CodeComponents/init_data/create.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE users (
-    username VARCHAR(50) PRIMARY KEY,
-    password CHAR(60) NOT NULL 
-);
-
--- CodeComponents/init_data/insert.sql
+++
@@ -1,1 +0,0 @@
-insert into users(username, password) values ('validUsername', '$2a$10$LmCHFRC6LZpuPNj9D9leXueyJicULBRcfVtADUGjOv/XTavy7av22');
