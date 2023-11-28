07fbb718475b5ca8e014426e3211c4e11f82e22e
kyndranelson <143553148+kyndranelson@users.noreply.github.com>
2023-11-27 18:32:35 -0700

    added navbar changes based on whether the user is logged in or not

--- CodeComponents/src/index.js
+++ CodeComponents/src/index.js
@@ -45,27 +45,39 @@ app.use(
       extended: true,
     })
   );
+
+app.use(express.static('resources'));
 /* END EXPRESS CONFIG */
 
 /* START ROUTES */
 app.get('/', (req, res) => {
+  res.render('partials/menu', {
+    user: req.user
+  });
+});
+
+app.get('/', (req, res) => {
   // Use the res.redirect method to redirect the user to the /login endpoint
-  res.render('/discover')
+  res.redirect('/discover')
 });
 // Discover route
 app.get('/discover', async (req, res) => {
   try {
-    // Fetch top-rated movies from TMDb using API key from .env
-    const apiKey = process.env.API_KEY;
-    const response = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`);
-    
-    // Extract relevant data
-    const topMovies = response.data.results || [];
+    const tmdbEndpoint = 'https://api.themoviedb.org/3/discover/movie';
+    const params = {
+      api_key: process.env.API_KEY,
+      language: 'en-US',
+      sort_by: 'popularity.desc',
+      page: 1,
+    };
 
-    // Render the discover page with top movies
+    const response = await axios.get(tmdbEndpoint, { params });
+
+
+    const topMovies = response.data.results.slice(0, 10); // Adjust the number of movies as needed
     res.render('pages/discover', { topMovies });
   } catch (error) {
-    console.error(error);
+    console.error('Error fetching data from TMDb:', error);
     res.status(500).send('Internal Server Error');
   }
 });
@@ -83,6 +95,44 @@ app.get('/register', (req, res) => {
 // TEST ROUTE
 app.get('/welcome', (req, res) => {
     res.json({status: 'success', message: 'Welcome!'});
+});
+
+app.get('/saved_movies', async (req, res) => {
+  try {
+    const username = req.session.user.username;
+    // TODO: Query the database for saved movies
+    const savedMovies = await db.any(
+      "SELECT sm.* FROM movies sm JOIN saved_to_users stu ON sm.id = stu.movie_id WHERE stu.username = $1",
+      [username]
+    );
+
+    console.log(savedMovies)
+
+    // Render the discover page with saved movies
+    res.render('pages/savedMovies', { savedMovies });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.get('/watched_movies', async (req, res) => {
+  try {
+    const username = req.session.user.username;
+    // TODO: Query the database for saved movies
+    const watchedMovies = await db.any(
+      "SELECT sm.* FROM movies sm JOIN watched_to_users stu ON sm.id = stu.movie_id WHERE stu.username = $1",
+      [username]
+    );
+
+    console.log(watchedMovies)
+
+    // Render the discover page with saved movies
+    res.render('pages/watchedMovies', { watchedMovies });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 // LOGIN ROUTE
@@ -111,6 +161,90 @@ app.post('/login', async (req, res) => {
     } catch (err) {
         res.render('pages/login', { error: 'Error when contacting database' });
     }
+});
+
+//REGISTER ROUTE
+app.post('/register', async (req, res) => {
+  //hash the password using bcrypt library
+  const hash = await bcrypt.hash(req.body.password, 10);
+
+  const user = await db.oneOrNone('SELECT * FROM users WHERE username = $1', [req.body.username]);
+
+  if (user) {
+      res.redirect('/login');
+      return;
+  }
+
+  db.tx(async (t) => {
+    await t.none(
+      "INSERT INTO users(username, password) VALUES ($1, $2);",
+      [req.body.username, hash]
+    );
+
+    res.render("pages/login");
+  }) 
+    .catch((err) => {
+      res.render("pages/register", {
+        error: true,
+        message: err.message,
+      });
+    });
+});
+
+// SAVE MOVIE
+app.post('/save_movie', async (req, res) => {
+  try {
+      const { title, release_date, genre, vote_average, overview, image_url } = req.body;
+      const username = req.session.user.username;
+
+      const existingMovie = await db.oneOrNone('SELECT id FROM movies WHERE title = $1', [title]);
+
+      let movieId;
+      if (existingMovie) {
+          movieId = existingMovie.id;
+      } else {
+          const insertedMovie = await db.one(
+              'INSERT INTO movies (title, release_date, genre, vote_average, overview, image_url) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id',
+              [title, release_date, genre, vote_average, overview, image_url]
+          );
+          movieId = insertedMovie.id;
+      }
+
+      await db.none('INSERT INTO saved_to_users (username, movie_id) VALUES ($1, $2)', [username, movieId]);
+
+      res.status(200).json({ message: "Movie saved successfully." });
+  } catch (error) {
+      console.error('Error saving movie:', error);
+      res.status(500).json({ error: "An error occurred while saving the movie." });
+  }
+});
+
+// WATCH MOVIE
+app.post('/watch_movie', async (req, res) => {
+  try {
+      const { title, release_date, genre, vote_average, overview, image_url } = req.body;
+      const username = req.session.user.username;
+
+      const existingMovie = await db.oneOrNone('SELECT id FROM movies WHERE title = $1', [title]);
+
+      let movieId;
+      if (existingMovie) {
+          movieId = existingMovie.id;
+      } else {
+          const insertedMovie = await db.one(
+              'INSERT INTO movies (title, release_date, genre, vote_average, overview, image_url) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id',
+              [title, release_date, genre, vote_average, overview, image_url]
+          );
+          movieId = insertedMovie.id;
+      }
+
+      await db.none('INSERT INTO watched_to_users (username, movie_id) VALUES ($1, $2)', [username, movieId]);
+
+      res.status(200).json({ message: "Movie saved successfully." });
+  } catch (error) {
+      console.error('Error saving movie:', error);
+      res.status(500).json({ error: "An error occurred while saving the movie." });
+  }
 });
 
 /* END ROUTES */
